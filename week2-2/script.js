3a31a3ed29728e395ddd8b7342a7300f4c8c096e
Minseo Kang <rachel5640@naver.com>
2023-10-18 06:38:23 +0900

    🎨: 파일 구조 정리,reset css 생성

---
+++ week2-2/script.js
@@ -0,0 +1,10 @@
+import { historyList } from './Data.js';
+
+function accountdetails(list){
+    const historyWrapper = document.getElementsByClassName("history-wrapper")//내역 영역 전체를 차지하는 부모 노드
+    const historyTemplate = document.getElementById("history-template");//컨텐츠를 차지하는 템플릿
+
+    // historyWrapper.replaceChildren();
+    // list.forEach
+
+}
