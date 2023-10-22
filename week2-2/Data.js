8d282061c29c0eb8d7d89493643540ca2189c8d5
Minseo Kang <rachel5640@naver.com>
2023-10-23 07:08:36 +0900

    ✨:초기 데이터 동적 생성

--- week2-2/Data.js
+++ week2-2/Data.js
@@ -1,6 +1,5 @@
-export const historyList = [
+export const HISTORY_DATA = [
   {
-    id: 1,
     category: "식비",
     content: "청년다방 건대점",
     amount: [{ name: +50000, status: "income" }],
@@ -8,22 +7,18 @@ export const historyList = [
   },
 
   {
-    id: 2,
     category: "교통비",
     content: "지하철 요금",
     amount: [{ name: -40000, status: "outcome" }],
     deletion: "x",
   },
-
   {
-    id: 3,
     category: "식비",
     content: "라밥 김밥구매",
     amount: [{ name: -5000, status: "outcome" }],
     deletion: "x",
   },
   {
-    id: 4,
     category: "알바비",
     content: "레디피부과",
     amount: [{ name: +300000, status: "income" }],
--- week2-2/script.js
+++ week2-2/script.js
@@ -1,64 +1,84 @@
-// import { historyList } from "./Data.js";
+import { HISTORY_DATA } from "./Data.js";
 
-// const historylistForm = document.getElementById("history-template");
-// const
+let historyData = [];
+const initBalance = 0;
 
-// function accountdetails(list) {
-//   const historySection = document.getElementsByClassName("history-wrapper"); //내역 영역 전체를 차지하는 부모 노드
-//   const historyTemplate = document.getElementById("history-template"); //컨텐츠를 차지하는 템플릿
+window.onload = () => {
+  const historyData = JSON.parse(localStorage.getItem("history_data")); // localstorage 저장된 목록 가져오기
+  mylist(historyData);
+};
 
-//   historyWrapper.replaceChildren();
-//     list.forEach((category, content, amount, button) => {
-//       let historyListNewHtml = accountdetails()
-//   } ));
-// }
+function mylist(list) {
+  const historySection = document.getElementById("historylist-wrapper"); //타겟
+  const historyTemplate = document.getElementById("history-template"); //템플릿
+  historySection.innerHtml = "";
+  list.forEach((item) => {
+    let todocontent = historyTemplate.content.cloneNode(true);
 
-// 모달 열기
-function modalOpen() {
-  document.querySelector(".modal_wrap").style.display = "block";
-  document.querySelector(".modal_background").style.display = "block";
+    todocontent.querySelector("#category").textContent = item.category;
+    todocontent.querySelector("#content").textContent = item.content;
+    todocontent.querySelector("#amount").textContent = item.amount[0].name;
+
+    historySection.appendChild(todocontent);
+  });
+
+  //모달 입력폼
+  const inputForm = document.getElementById("input-form");
+  const inputPrice = inputForm.querySelector(".price-input");
+  const inputContent = inputForm.querySelector(".content-input");
+  const myHistory = document.getElementById("historylist-wrapper");
+
+  const lists_key = "데이터";
+  let lists = []; //빈 배열임을 표시
+
+  window.onload = () => {
+    localStorage.setItem(lists_key, JSON.stringify(lists));
+  };
+
+  function makeList(contentPrice) {
+    const wrapper = document.createElement("ul");
+
+    const content = document.createElement("li");
+    content.innerText = contentPrice;
+
+    const deletion = document.createElement("button");
+    deletion.innerText = "x";
+    deletion.addEventListener("click", deleteHistory);
+
+    wrapper.appendChild(content);
+    wrapper.appendChild(deletion);
+    myHistory.appendChild(wrapper);
+  }
+
+  function handleHistorySubmit(event) {
+    event.preventDefault(); // 새로고침 방지
+
+    const contentPrice = inputPrice.value; //새로운 변수에 복사
+    const contentValue = inputContent.value; //
+    //   console.log(`가격: ${contentPrice}`);
+    //   console.log(`내용: ${contentValue}`);
+    lists.push(contentPrice);
+    makeList(contentPrice); //makelist로 전송
+    //   saveList(contentPrice); //savelist로 전송
+  }
+
+  inputForm.addEventListener("submit", handleHistorySubmit);
+  function deleteHistory(event) {
+    const ul = event.target.parentElement;
+    ul.remove();
+  }
+
+  // 모달 열기
+  function modalOpen() {
+    document.querySelector(".modal_wrap").style.display = "block";
+    document.querySelector(".modal_background").style.display = "block";
+  }
+
+  function modalClose() {
+    document.querySelector(".modal_wrap").style.display = "none";
+    document.querySelector(".modal_background").style.display = "none";
+  }
+
+  document.querySelector("#modal_btn").addEventListener("click", modalOpen);
+  document.querySelector("#modal_close").addEventListener("click", modalClose);
 }
-
-// 모달 끄기
-function modalClose() {
-  document.querySelector(".modal_wrap").style.display = "none";
-  document.querySelector(".modal_background").style.display = "none";
-}
-
-//버튼 클릭리스너 달기
-document.querySelector("#modal_btn").addEventListener("click", modalOpen);
-document.querySelector("#modal_close").addEventListener("click", modalClose);
-
-function deleteHistory(event) {
-  const ul = event.target.parentElement;
-  ul.remove();
-}
-
-//모달 입력 정보
-const inputForm = document.getElementById("input-form");
-const inputPrice = inputForm.querySelector("input");
-const myHistory = document.getElementById("historylist-wrapper");
-
-function makeList(newHistory) {
-  const wrapper = document.createElement("ul");
-
-  const content = document.createElement("li");
-  content.innerText = newHistory;
-
-  const deletion = document.createElement("button");
-  deletion.innerText = "x";
-  deletion.addEventListener("click", deleteHistory);
-
-  wrapper.appendChild(content);
-  wrapper.appendChild(deletion);
-  myHistory.appendChild(wrapper);
-}
-
-function handleHistorySubmit(event) {
-  event.preventDefault(); // 새로고침 방지
-  const newHistory = inputPrice.value; //새로운 변수에 복사
-  inputPrice.value = "";
-  makeList(newHistory); //makelist로 전송
-}
-
-inputForm.addEventListener("submit", handleHistorySubmit);
