5e1802ab48f5a0fad3d6967380d685b9d675f915
Minseo Kang <rachel5640@naver.com>
2023-10-22 12:13:15 +0900

    🔧:가격 입력창 html 추가

--- week2-2/Data.js
+++ week2-2/Data.js
@@ -1,38 +1,32 @@
 export const historyList = [
-    {   
-        id: 1,
-        category: '식비',
-        content: '청년다방 건대점',
-        amount: [
-            { name: +50000, status: "income"}
-        ],
-        deletion: "x"},
+  {
+    id: 1,
+    category: "식비",
+    content: "청년다방 건대점",
+    amount: [{ name: +50000, status: "income" }],
+    deletion: "x",
+  },
 
-    {   
-        id: 2,
-        category: '교통비',
-        content: '지하철 요금',
-        amount: [
-            { name: -40000, status: "outcome"}
-        ],
-        deletion: "x"},
+  {
+    id: 2,
+    category: "교통비",
+    content: "지하철 요금",
+    amount: [{ name: -40000, status: "outcome" }],
+    deletion: "x",
+  },
 
-    {   
-        id: 3,
-        category: '식비',
-        content: '라밥 김밥구매',
-        amount: [
-            { name: -5000, status: "outcome"}
-        ],
-        deletion: "x"},
-    {   
-        id: 4,
-        category: '알바비',
-        content: '레디피부과',
-        amount: [
-            { name: +300000, status: "income"}
-        ],
-        deletion: "x"}
-
-    ]
-    
+  {
+    id: 3,
+    category: "식비",
+    content: "라밥 김밥구매",
+    amount: [{ name: -5000, status: "outcome" }],
+    deletion: "x",
+  },
+  {
+    id: 4,
+    category: "알바비",
+    content: "레디피부과",
+    amount: [{ name: +300000, status: "income" }],
+    deletion: "x",
+  },
+];
--- week2-2/script.js
+++ week2-2/script.js
@@ -1,10 +1,14 @@
-import { historyList } from './Data.js';
+// import { historyList } from "./Data.js";
 
-function accountdetails(list){
-    const historyWrapper = document.getElementsByClassName("history-wrapper")//내역 영역 전체를 차지하는 부모 노드
-    const historyTemplate = document.getElementById("history-template");//컨텐츠를 차지하는 템플릿
+// const historylistForm = document.getElementById("history-template");
+// const
 
-    // historyWrapper.replaceChildren();
-    // list.forEach
+// function accountdetails(list) {
+//   const historySection = document.getElementsByClassName("history-wrapper"); //내역 영역 전체를 차지하는 부모 노드
+//   const historyTemplate = document.getElementById("history-template"); //컨텐츠를 차지하는 템플릿
 
-}
+//   historyWrapper.replaceChildren();
+//     list.forEach((category, content, amount, button) => {
+//       let historyListNewHtml = accountdetails()
+//   } ));
+// }
