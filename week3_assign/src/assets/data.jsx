57cb72d3063868e610606c676e29d5ae20f0d5af
Minseo Kang <rachel5640@naver.com>
2023-11-11 12:23:14 +0900

    data: 데이터 변경

--- week3_assign/src/assets/data.jsx
+++ week3_assign/src/assets/data.jsx
@@ -1,30 +1,18 @@
 const data = [
   [
-    ["블랙미러", "에밀리, 파리에 가다", "루머의 루머의 루머"],
-    ["최유리 - 바람", "데이식스 - hihello", "데이먼스이어 - yours"],
-    ["옥상달빛 - 수고했어 오늘도", "하현상 - 등대", "KozyPop - Dezavu"],
+    ["111", "121", "123"],
+    ["3311", "221", "222"],
+    ["333311", "312", "313"],
   ],
   [
-    ["스윗트홈", "부석순 - 파이팅해야지", "전소미 - fast forward"],
-    ["뉴진스 - Ditto", "EXO - 첫눈", "수지 - cape"],
-    ["STAYC - so what", "엔시티드림 - 미니카", "선미 - 보랏빛 밤"],
+    ["스윗트홈", "222", "ㅇㅇ"],
+    ["ㅇㅇ", "ㅇㅇ", "ㅇㅇ"],
+    ["ㅇㅇ", "ㅇㅇ", "ㅇㅇ"],
   ],
   [
-    [
-      "Taylor Swift - Shake it off",
-      "Sam Smith - Unholy",
-      "Justin Bieber, 라로이 - STAY",
-    ],
-    [
-      "Selena Gomez - lose you to love me",
-      "Justin - Bieber - off my face",
-      "5S0S - high",
-    ],
-    [
-      "Selena Gomez - Who says",
-      "Charlie Puth - That's hilarious",
-      "Why don't we - What am I",
-    ],
+    ["ㅇ", "ㅇㅇ", "ㅇㅇㅇ"],
+    ["ㅇㅇㅇ", "ㅇㅇㅇ", "ㅇㅇㅇ"],
+    ["ㅇㅇ", "ㅇㅇ", "ㅇㅇ"],
   ],
 ];
 
