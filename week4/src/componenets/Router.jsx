b83b0b83e9866ea1a6a48a414de975d421f0b4a5
Minseo Kang <rachel5640@naver.com>
2023-11-15 01:22:24 +0900

    style: theme.js 작성

--- week4/src/componenets/Router.jsx
+++ week4/src/componenets/Router.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "../pages/login";
 import SignUp from "../pages/signup";
 import UserID from "../pages/userID";
@@ -7,9 +9,9 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/post/:postId" element={<PostDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage/:userId" element={<UserID />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ week4/src/sytles/globalStyle.js
@@ -0,0 +1,20 @@
+const colors = {
+    white: "#ffffff",
+    gray1: "#F2F3F7",
+    gray2: "#4E515C",
+    yellow: "#faeD62",
+    black: "#101011"
+    
+
+
+};
+
+const font = {
+    spoka: font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+};
+
+const theme = {
+    colors, font
+};
+
+export default theme;
