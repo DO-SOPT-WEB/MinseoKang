92f834e2e03540e96133ddf459ba213a8bd1dc66
Minseo Kang <rachel5640@naver.com>
2023-11-15 01:31:44 +0900

    style: 전역 reset, blobal style 연결

--- week4/src/main.jsx
+++ week4/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyle from "./styles/globalStyle.js";
+import { ThemeProvider } from "styled-componenets";
+import theme from "./styles/theme.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
