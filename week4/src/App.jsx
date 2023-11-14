ef2c3ffc951527101298116e52705a3385763f45
Minseo Kang <rachel5640@naver.com>
2023-11-14 18:38:58 +0900

    init: 프로젝트 생성

---
+++ week4/src/App.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+function App() {
+  return (
+    <>
+      <div>우와</div>
+    </>
+  );
+}
+
+export default App;
---
+++ week4/src/componenets/contentBox.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const contentBox = styled.section`
+  width: 30vw;
+  height: 50rem;
+  background-color: pink;
+`;
+export default contentBox;
